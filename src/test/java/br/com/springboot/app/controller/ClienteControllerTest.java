2199dc48b915d0e522d364ca46b988e754f4b41b
DanielLuis <dluis2007@gmail.com>
2016-12-26 17:54:58 -0200

    Mudança Resource

--- src/test/java/br/com/springboot/app/controller/ClienteControllerTest.java
+++ src/test/java/br/com/springboot/app/controller/ClienteControllerTest.java
@@ -1,18 +1,19 @@
 package br.com.springboot.app.controller;
 
 
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,14 +24,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.springboot.app.controller.ClienteController;
 import br.com.springboot.app.domain.Cliente;
 import br.com.springboot.app.service.ClienteService;
 import br.com.springboot.app.support.BaseTest;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ClienteController.class)
-public class ClienteControllerTest extends BaseTest{
+public class ClienteControllerTest{
     
 	@Autowired
     public MockMvc mockMvc;
@@ -38,34 +38,58 @@ public class ClienteControllerTest extends BaseTest{
     @MockBean
     private ClienteService clienteService;
 
+    @MockBean
+    private Cliente cliente;
 
     private String json;
+    
+    @Autowired
+    public ObjectMapper objectMapper;
 
+    private static String URL_CLIENTE = "/clientes/";
+    
+
+    public <T>  String obterJson(T object) {
+	    String json = null;
+
+		try {
+			json = objectMapper.writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return json;
+	}
     @Before
     public void setUp() {
-    	
-    	cliente = getCliente(null);
+    	cliente = new Cliente(Long.valueOf(1),"Daniel");
     	
     	json = obterJson(cliente);
-    	
     }
 
 
 
 	
     
-//    @Test
-//    public List<Cliente> findAllTest(){
+    @Test
+    public void findAllTest() throws Exception{
+    	Cliente cliente1 = new Cliente("Daniel"); 
+    	Cliente cliente2 = new Cliente("Fabio"); 
     	
-//    	given(this.clienteService.findAll())
-//        .willReturn(new VehicleDetails("Honda", "Civic"));
-//
-//    this.mvc.perform(get("/sboot/vehicle")
-//        .accept(MediaType.APPLICATION_JSON))
-//        .andExpect(status().isOk())
-//        .andExpect(content().string("Honda Civic"));
-//		return ;
-//	}
+    	List<Cliente> clientes = Arrays.asList(cliente1,cliente2);
+    	
+    	
+    	when(clienteService.findAll()).thenReturn(clientes);
+    	
+    	
+    	this.mockMvc.perform(get(URL_CLIENTE)
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is(1)))
+        .andExpect(jsonPath("$.description", is("Lorem ipsum")))
+        .andExpect(jsonPath("$.title", is("Foo")));
+	}
 
 //	public Cliente findOneTest(long id){
 //		return clienteRepository.findOne(id);
@@ -76,10 +100,17 @@ public class ClienteControllerTest extends BaseTest{
 //	
     @Test
     public void saveClienteTest() throws Exception{
-			this.mockMvc.perform(post("/clientes/add")
-			    .contentType(MediaType.APPLICATION_JSON)
-			    .content(json))
-			    .andExpect(status().isOk());
+    	
+//    	when(clienteService.saveCliente(any(Cliente.class))).thenReturn(getCliente(Long.valueOf(null)));
+//    	
+//    	
+//			this.mockMvc.perform(post("/clientes/add")
+//			    .contentType(MediaType.APPLICATION_JSON)
+//			    .content(json))
+//			    .andExpect(status().isOk());
+//			
+//			
+//		verif
     	
 	}
 //	@Transactional(propagation=Propagation.REQUIRED)
